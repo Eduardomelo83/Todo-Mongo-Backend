870a66a5cfb6b96777ed39601d938d8f7d5d7661
eduardomelo83 <eduardomelopro83@gmail.com>
2020-12-25 07:40:40 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,8 @@
-const express = require('express');
-const moogoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const Mongo = require('./services/Mongo');
+const Server = require('./server');
 
-const todoRouter = require('./routes/todoRouter');
-const app = express();
-
-app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
-    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
-        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
-    else //Se a requisição já é HTTPS 
-        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
-});
-
-app.use(cors());
-app.use(express.json());
+Mongo();
+Server();
 
 
 
-moogoose.connect(process.env.MONGO, {useUnifiedTopology: true, useNewUrlParser: true});
-let db = moogoose.connection;
-
-db.on('error', () => {console.log('houve um erro')});
-db.once('open', () => {console.log('banco carregado')});
-
-app.use('/', todoRouter);
-
-app.listen(process.env.PORT, () => {
-    console.log(`server running on port ${process.env.PORT}`);
-
-})
